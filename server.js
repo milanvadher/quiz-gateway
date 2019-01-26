521ba39a216d74eca1202364ca7fe21f2fead584
Milan Vadher <milanvadher1996@gmail.com>
2019-01-26 16:20:36 +0530

    add expception route from token

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ server.use(restifyPlugins.acceptParser(server.acceptable));
 server.use(restifyPlugins.queryParser({ mapParams: true }));
 server.use(restifyPlugins.fullResponse());
 server.use(function (req, res, next) {
-    if (req.url === '/login' || req.url === '/register') return next();
+    if (req.url === '/login' || req.url === '/validate_user' || req.url === '/register' || req.url === '/forgot_password' || req.url === '/update_password') return next();
 
     // check header or url parameters or post parameters for token
     const token = req.headers['x-access-token'] || req.query.token;
