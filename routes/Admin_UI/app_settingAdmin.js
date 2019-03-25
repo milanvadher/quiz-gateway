6f39cf49613ac33f128ef22b19808a84a958030d
Milan Vadher <milanvadher1996@gmail.com>
2019-03-25 21:34:29 +0530

    add ios appversion

--- routes/Admin_UI/app_settingAdmin.js
+++ routes/Admin_UI/app_settingAdmin.js
@@ -29,7 +29,7 @@ exports.get = async function (req, res, next) {
 exports.get_appversion= async function(req,res,next)
 {
     try {
-        let application= await  ApplicationSetting.findOne({},"appversion");
+        let application= await  ApplicationSetting.findOne({},"appversion ios_appversion");
         res.send(200, application);
         next();
     } catch (error) {
